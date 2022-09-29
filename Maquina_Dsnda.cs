0f7a9c4db5a29dd2fb3f05fe2f101fb84767c032
Rafagf1408 <113957083+Rafagf1408@users.noreply.github.com>
2022-09-28 21:37:31 -0500

    Update Maquina_Dsnda.cs

--- Maquina_Dsnda.cs
+++ Maquina_Dsnda.cs
@@ -25,11 +25,13 @@ namespace ConsoleApp2
             NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
             DriveInfo[] allDrives = DriveInfo.GetDrives();
 
-            
+            var arch = new List<string> { "Architecture  -  x86", "Architecture  -  MIPS", "Architecture  -  Alpha", "Architecture  -  POWERPC", "4","Architecture  -  ARM", "Architecture  -  ia64(Itanium-based systems)", "7", "8", "Architecture  -  x64" };
 
+
+            Esc("--------------------PROCESSOR--------------------------------");
             if (myProcessorObject.Get() == null)
             {
-                Console.WriteLine("Error de Procesador");
+                Esc("Error de Procesador");
                 SystemSounds.Hand.Play();
             }
             else
@@ -37,39 +39,19 @@ namespace ConsoleApp2
 
                 foreach (ManagementObject obj in myProcessorObject.Get())
                 {
-                    Console.WriteLine("Name  -  " + obj["Name"]);
-                    switch (obj["Architecture"].ToString())
-                    {
-                        case "0":
-                            Console.WriteLine("Architecture  -  x86");
-                            break;
-                        case "1":
-                            Console.WriteLine("Architecture  -  MIPS");
-                            break;
-                        case "2":
-                            Console.WriteLine("Architecture  -  Alpha");
-                            break;
-                        case "3":
-                            Console.WriteLine("Architecture  -  PowerPC");
-                            break;
-                        case "5":
-                            Console.WriteLine("Architecture  -  ARM");
-                            break;
-                        case "6":
-                            Console.WriteLine("Architecture  -  ia64(Itanium-based systems)");
-                            break;
-                        case "9":
-                            Console.WriteLine("Architecture  -  x64");
-                            break;
-                    }
-                    Console.WriteLine("PROCESADOR  OK!\n\n");
+                    Esc("Name  -  " + obj["Name"]);
+                    int ind = int.Parse(obj["Architecture"].ToString());
+                    Console.WriteLine(arch[ind]);
+                    Esc("PROCESADOR  OK!");
 
 
                 }
+                Esc("----------------------------------------------------");
+                Esc("----------------------VIDEOCARD------------------------------");
 
                 if (myVideoObject.Get() == null)
                 {
-                    Console.WriteLine("Error de Tarjeta de Video");
+                    Esc("Error de Tarjeta de Video");
                     SystemSounds.Hand.Play();
                 }
                 else
@@ -77,16 +59,17 @@ namespace ConsoleApp2
 
                     foreach (ManagementObject obj in myVideoObject.Get())
                     {
-                        Console.WriteLine("Name  -  " + obj["Name"]);
+                        Esc("Name  -  " + obj["Name"]);
                         if (obj["Status"].Equals("OK"))
                         {
-                            Console.WriteLine("TARJETA DE VIDEO  OK!!!\n");
+                            Esc("TARJETA DE VIDEO  OK!!!\n");
+                            Esc("----------------------------------------------------");
                             string strComputer = ".";
                             ManagementScope scope = new ManagementScope(@"\\" + strComputer + @"\root\cimv2");
                             ObjectQuery queryUsbControllers = new ObjectQuery("Select * From Win32_USBControllerDevice");
                             ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, queryUsbControllers);
                             ManagementObjectCollection usbControllers = searcher.Get();
-
+                            Esc("-------------------MOUSE AND KEYBOARD-------------------------");
                             foreach (ManagementObject usbController in usbControllers)
                             {
                                 string dependent = (string)usbController["Dependent"];
@@ -102,15 +85,15 @@ namespace ConsoleApp2
                                     Console.WriteLine("description = {0}", usbDevice["Description"]);
 
                                 }
-                                if (usbDevices.Count == null)
+                                if (usbDevices.Count == 0)
                                 {
-                                    Console.WriteLine("ERROR Mouse");
+                                    Esc("ERROR Mouse");
                                     SystemSounds.Hand.Play();
                                     break;
                                 }
                                 else
                                 {
-                                    Console.WriteLine("MOUSE  OK!\n");
+                                    Esc("MOUSE  OK!\n");
                                     break;
                                 }
 
@@ -128,20 +111,18 @@ namespace ConsoleApp2
                                 foreach (ManagementObject usbDevice in usbDevices)
                                 {
                                     Console.WriteLine("description = {0}", usbDevice["Description"]);
-
                                 }
 
-                                if (usbDevices.Count == null)
+                                if (usbDevices.Count == 0)
                                 {
-                                    Console.WriteLine("ERROR TECLADO");
+                                    Esc("ERROR TECLADO");
                                     erro = true;
                                     SystemSounds.Hand.Play();
                                     break;
                                 }
                                 else
                                 {
-                                    Console.WriteLine("TECLADO  OK!\n");
-
+                                    Esc("TECLADO  OK!\n");
                                     break;
                                 }
 
@@ -154,124 +135,114 @@ namespace ConsoleApp2
                                     //Console.WriteLine(Int64.Parse(obj2["TotalVisibleMemorySize"].ToString()));
                                     //Console.WriteLine("Free Physical Memory: {0} KB", obj2["FreePhysicalMemory"]);
                                     if ((Int64.Parse(obj2["TotalVisibleMemorySize"].ToString()) >= 1) && (Int64.Parse(obj2["FreePhysicalMemory"].ToString()) >= 1)) {
-                                        erro = false;
+                                        erro = true;
                                         break;
                                     }
                                     
                                     
                                     
                                 }
-                                if (!erro)
+                                Esc("----------------------------------------------------");
+                                if (erro)
                                 {
+
+                                    Esc("-----------------------AUDIO---------------------------");
                                     foreach (ManagementObject obj3 in myAudioObject.Get())
                                     {
-                                        /*Console.WriteLine("Name  -  " + obj3["Name"]);
-                                        Console.WriteLine("ProductName  -  " + obj3["ProductName"]);
-                                        Console.WriteLine("Availability  -  " + obj3["Availability"]);
-                                        Console.WriteLine("DeviceID  -  " + obj3["DeviceID"]);
-                                        Console.WriteLine("PowerManagementSupported  -  " + obj3["PowerManagementSupported"]);*/
-                                        Console.WriteLine("TajAudioStatus: " + obj3["Status"]);
+                                      Console.WriteLine("TajAudioStatus: " + obj3["Status"]);
                                         if (obj3["Status"].Equals("OK")) {
-                                            erro = true;
+                                            erro = false;
                                             break;
                                         }
 
-                                        /*Console.WriteLine("StatusInfo  -  " + obj3["StatusInfo"]);
-                                        Console.WriteLine(String.Empty.PadLeft(obj3["ProductName"].ToString().Length, '='));*/
                                     }
-                                    if (erro)
+                                    if (!erro)
                                     {
                                         SystemSounds.Exclamation.Play();
                                        
                                     } else {
                                         
-                                        Console.WriteLine("ERROR AUDIO");
+                                        Esc("ERROR AUDIO");
                                         
                                     }
+                                    Esc("------------------------------------------------------");
+                                    Esc("-----------------------RED---------------------------");
                                     if (nics == null || nics.Length < 1)
                                     {
-                                        Console.WriteLine("  No network interfaces found.");
+                                        Esc("  No network interfaces found.");
                                     }
                                     else
                                     {
-                                        Console.WriteLine("-----------------------RED---------------------------");
+                                       
                                         foreach (NetworkInterface adapter in nics)
                                         {
                                             IPInterfaceProperties properties = adapter.GetIPProperties();
-                                            Console.WriteLine(adapter.Description);
+                                            Esc(adapter.Description);
                                             if (adapter.OperationalStatus.ToString()=="Up") { Console.WriteLine("Operational status:    OK"); }
                                           
                                         }
-                                        Console.WriteLine("----------------------------------------------------");
+                                        Esc("----------------------------------------------------");
                                     }
 
-                                    Console.WriteLine("\n    ");
+                                    Esc("----------------------HDD & SSD------------------------------");
                                     foreach (DriveInfo d in allDrives)
                                     {
                                         Console.WriteLine("Drive {0}", d.Name);
                                         Console.WriteLine("  Drive type: {0}", d.DriveType);
                                         if (d.IsReady == true)
                                         {
-                                            erro= false;
-                                            Console.WriteLine("Unidad Existente y lista para su utilizacion");
+                                            erro= true;
+                                            Esc("Unidad Existente y lista para su utilizacion");
                                             Console.WriteLine("  Total size of drive:            {0, 15} bytes \n", d.TotalSize);
-
                                         }
                                         else
                                         {
-                                            Console.WriteLine(" Unidad desabilitada Y/O Error");
+                                            Esc(" Unidad desabilitada Y/O Error");
                                             SystemSounds.Hand.Play();
                                         }
                                     }
-                                    if (!erro) {
+                                    Esc("--------------------------------------------------------------");
+                                    Esc("-------------------------OS-------------------------------------");
+                                    if (erro) {
                                         foreach (ManagementObject obj2 in myOperativeSystemObject.Get())
                                         {
-                                            Console.WriteLine("SYSTEM OPERATIVE:  " + obj2["Caption"]);
-                                            
-                                            /*Console.WriteLine("OSType  -  " + obj2["OSType"]);(Desconocido(0),Otros(1),MACOS(2),ATTUNIX(3),DGUX(4),DECNT(5),Unix digital(6),OpenVMS(7),
-                                             *HPUX(8),AIX(9),MVS(10),OS400(11),SO / 2(12),JavaVM(13),MSDOS(14),WIN3x(15),WIN95(16),WIN98(17),WINNT(18),WINCE(19),NCR3000(20),NetWare(21),
-                                             *OSF(22),DC / OS(23),UNIX de Reliant(24),SCO UnixWare(25),SCO OpenServer(26),Sequent(27),IRIX(28),Solaris(29),SunOS(30),U6000(31),ASERIES(32),
-                                             *TándemNSK(33),TándemNT(34),BS2000(35),LINUX(36),Lynx(37),XENIX(38),VM / ESA(39),UNIX interactivo(40),BSDUNIX(41),FreeBSD(42),NetBSD(43),GNU Hurd(44),
-                                             *OS9(45),Kernel mach(46),Inferno(47),QNX(48),EPOC(49),IxWorks(50),VxWorks(51),MiNT(52),BeOS(53),HP MPE(54),NextStep(55),PalmPilot(56),Rhapsody(57),
-                                             Windows 2000(58),Dedicado(59),SO / 390(60),VSE(61),TPF(62)/
+                                            Esc("SYSTEM OPERATIVE:  " + obj2["Caption"]);
+                                          
                                         }
                                     } else {
                                         SystemSounds.Hand.Play(); 
-                                        Console.WriteLine("NO HAY SISTEMA OPERATIVO"); }
-                                    
+                                        Esc("NO HAY SISTEMA OPERATIVO"); }
+                                    Esc("--------------------------------------------------------------");
+
                                 }
                                 else {
                                     SystemSounds.Hand.Play();
-                                    Console.WriteLine("ERROR RAM");
+                                    Esc("ERROR RAM");
                                     Console.ReadLine();
                                 }
-
-                                
                             }
                             else
                             {
                                 SystemSounds.Hand.Play();
-                                Console.WriteLine("Presione una tecla para continuar");
+                                Esc("Presione una tecla para continuar");
                                 Console.ReadLine();
                             }
                         }
                         else
                         {
-                            Console.WriteLine(" ERROR EN TARJETA DE VIDEO");
+                            Esc(" ERROR EN TARJETA DE VIDEO");
                             SystemSounds.Hand.Play();
                             SystemSounds.Hand.Play();
                         }
-
-
-
                     }
-
                 }
-
-
-
+                Esc("---------------------------END-------------------------PRESS ENTER KEY----------");
                 Console.ReadLine();
             }
+        }
+        static void Esc(string mensaje)
+        {
+            Console.WriteLine(mensaje);
         }
     }
 }
